ebc62fa585ff47dc279400ba1603ec11a5185565
Eduardo Inerarte <Eduardo.Inerarte@yrbrands.com>
2020-04-07 11:29:51 +0200

    forms

---
+++ js/js.js
@@ -0,0 +1,28 @@
+let elemnts = [{
+    'Denmark': [
+        "Es", "En", "SW"
+    ]
+}, {
+    'Denmark': [
+        "Es", "En", "SW"
+    ]
+}, {
+    'Denmark': [
+        "Es", "En", "SW"
+    ]
+}, {
+    'Denmark': [
+        "Es", "En", "SW"
+    ]
+}, {
+    'Denmark': [
+        "Es", "En", "SW"
+    ]
+}]
+
+
+
+
+
+document.getElementById("lang").nodeValue("daujak");
+document.getElementById("countries");
